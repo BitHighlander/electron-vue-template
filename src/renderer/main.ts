286eb3016883d7d401e74d24a551eda605ba1e14
highlander <bithighlander@gmail.com>
2022-05-16 15:21:38 -0600

    subscribe to ipcRenderer

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import {ipcRenderer} from './electron'
+
 const app = createApp(App);
+console.log(ipcRenderer)
+ipcRenderer.on('@test/state', (_event, data) => {
+    console.info('@test/state', data)
+})
+
+
 
 app.mount('#app');
